8543b9bb953d09519f689f85e83d208c76c69108
s4saikat87 <saikatg@fi-tek.co.in>
2022-08-10 18:43:46 +0530

    added

---
+++ GwesReportApi/Controllers/DocController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using GwesReportApi.Inventory;
+using GwesReportApi.Models;
+using Microsoft.EntityFrameworkCore;
+using GwesReportApi.Helpers;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+
+namespace GwesReportApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class DocController : ControllerBase
+    {
+        private IConfiguration _config;
+        private readonly GwesDbContext _context;
+        public DocController(IConfiguration config, GwesDbContext context)
+        {
+            _config = config;
+
+            _context = context;
+        }
+        
+        [HttpPost]
+        /*[Authorize]*/
+        public List<DocModel> Doc()
+        {
+            var currentUser = HttpContext.User;
+            var docs = _context
+                     .DbDocs
+                     .FromSqlRaw("exec AdvGetDoc")
+                     .ToList();
+            return docs;
+        }
+
+      
+    }
+}
---
+++ GwesReportApi/Models/GwesDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GwesReportApi.Models
+{
+    public class GwesDbContext:DbContext
+    {
+        public GwesDbContext()
+        {
+        }
+
+        public GwesDbContext(DbContextOptions<GwesDbContext> options) : base(options) { }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().HasNoKey();
+            modelBuilder.Entity<DocModel>().HasNoKey();
+        }
+
+        public DbSet<UserModel> DbUsers { get; set; }
+        public DbSet<DocModel> DbDocs { get; set; }
+    }
+}
