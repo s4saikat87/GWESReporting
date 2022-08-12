7bf66c42364daf273b71fd2e13fea0a207a09499
aU53RN4M3 <96877946+aU53RN4M3@users.noreply.github.com>
2022-08-12 12:06:35 +0530

    Create AccountProfile.cs

---
+++ GwesReportApi/Models/AccountProfile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GwesReportApi.Models
+{
+    public class AccountProfile
+    {
+        public int AcctId { get; set; }
+        //--Sort Fields
+        public string AccountNumber { get; set; }
+        public string AcctAlphaSortKey { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public string AcctTypeSortKey { get; set; }
+        public string Admin { get; set; }
+        public string Administrator { get; set; }
+        public string BranchName { get; set; }
+        public string InvestmentOfficer { get; set; }
+        public string InvOff { get; set; }
+        //-- Report Fields
+        public string InvObjective { get; set; }
+        public string InvAuthority { get; set; }
+        public string Capacity { get; set; }
+        public string IncptnDt { get; set; }
+        public decimal YTDGainLoss { get; set; }
+        public int GroupId { get; set; }
+        public string GroupSort { get; set; }
+        public string GroupName { get; set; }
+        public decimal TxCstAmt { get; set; }
+        public decimal Market { get; set; }
+        public int ExcldInd { get; set; }
+        public decimal Income { get; set; }
+        public decimal Yield { get; set; }
+        public decimal MarketPercent { get; set; }
+        public string RunDateTime { get; set; }
+        public bool PageNo { get; set; }
+        public bool IsCurrentInfoLine { get; set; }
+
+
+    }
+
+}
